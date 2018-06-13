64730c3e8d6d18261320c4274e556507b4e138a7
João Cavalcante <contato@kavalcante.com>
2018-06-13 18:29:45 -0300

    Link change

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -8,7 +8,7 @@ var companyInitals = 'Nutrir';
 // this may change based on your local or live server set
 var www = "";
 if(window.location.href.indexOf("www") > -1){ www = "www."};
-var serverPath = "http://localhost:8080";
+var serverPath = "https://assinatura.nutrir.agr.br";
 // custom video tutorial link
 var tutorialVideoLink = serverPath + "/assets/tutorial/lane-college-signature-tutorial_s.mp4";
 // defaults
